ad5a27b4be3bf1c1ffef3f1c7207c9e79d0e3380
Cristian Castaño <cristian.castano1@udea.edu.co>
2022-08-11 21:35:15 -0500

    feat: Add the Home

---
+++ src/components/molecules/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Category = ({ image, cate }) => (
+  <div className='flex flex-col items-center h-32'>
+    <div className='h-16 W-16'>
+      <img className=' pb-8' src={image} alt={cate} />
+    </div>
+    <p>{cate}</p>
+  </div>
+);
+
+export { Category };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import bannerImg from 'assets/icons/banner-blackfriday.png';
+import carroIcon from 'assets/icons/icon-carro.svg';
+import motoIcon from 'assets/icons/icon-moto.svg';
+import ropaIcon from 'assets/icons/icon-ropa.svg';
+import deporteIcon from 'assets/icons/icon-deporte.svg';
+import { Category } from 'components/molecules/Category';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='flex flex-col items-center'>
+      <img src={bannerImg} alt='Banner Black Friday' className='' />
+      <h1 className='text-lg font-bold md:text-2xl text-violet'>
+        ¿Qué estás buscando hoy?
+      </h1>
+      <div className='flex items-center justify-center gap-16 h-48'>
+        <Category image={carroIcon} cate='Carros' />
+        <Category image={motoIcon} cate='Motos' />
+        <Category image={ropaIcon} cate='Ropa' />
+        <Category image={deporteIcon} cate='Deporte' />
+      </div>
+    </div>
+  );
 }

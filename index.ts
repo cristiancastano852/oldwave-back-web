612778d118667d9ee09237c27457ebb3a4965797
Cristian Castaño <cristian.castano1@udea.edu.co>
2022-09-07 11:56:30 -0500

    add crud admin

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import routeProducts from './views/products/products'
+import routeAdmin from './views/admin/admin'
 
 const app = express()
 
 const port=process.env.PORT || 4000
 app.use(express.json())
 app.use(routeProducts)
+app.use(routeAdmin)
 
 
 const server = app.listen(port, () =>
